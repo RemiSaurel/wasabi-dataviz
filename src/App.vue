a7be76a35c7fabe7108a3d377493f1d7f6bef4fd
RemiSaurel <r.saurel5@gmail.com>
2023-09-10 12:16:38 +0200

    add: router + fix contstant / style

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,15 @@
 <script setup lang="ts">
-import { Ref, ref } from "vue";
-import { ALL_SONG_STEP, useSongList } from "./queries/song.queries.ts";
+import { useRouter } from "vue-router";
+import { onErrorCaptured } from "vue";
 
-const start: Ref<number> = ref(0);
-const id: Ref<string> = ref("");
-
-const { data: songList, isLoading: listLoading } = useSongList(start);
-
-const updateStart = (newStart: number) => {
-  if (newStart < 0) {
-    start.value = 0;
-  } else {
-    start.value = newStart;
-  }
-};
+const router = useRouter();
+onErrorCaptured((err, vm, info) => {
+  console.log(err, vm, info);
+});
 </script>
 
 <template>
-  <div>
-    <button @click="updateStart(start - ALL_SONG_STEP)">Previous</button>
-    start = {{ start }}
-    <button @click="updateStart(start + ALL_SONG_STEP)">Next</button>
-    <div v-if="listLoading">Loading...</div>
-    <div v-else-if="songList">
-      <table class="w-full text-sm text-left text-gray-700 mt-4">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-          <tr>
-            <th scope="col" class="px-6 py-3">Id</th>
-            <th scope="col" class="px-6 py-3">Title</th>
-            <th scope="col" class="px-6 py-3">Album</th>
-            <th scope="col" class="px-6 py-3">Album ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="bg-white border-b" v-for="song in songList" :key="song.id">
-            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-              {{ song._id }}
-            </td>
-            <td class="px-6 py-4">{{ song.title }}</td>
-            <td class="px-6 py-4">{{ song.albumTitle }}</td>
-            <td class="px-6 py-4">{{ song.id_album }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'https://wasabi.i3s.unice.fr/api/v1/';
+export const API_URL = "https://wasabi.i3s.unice.fr/api/v1";
