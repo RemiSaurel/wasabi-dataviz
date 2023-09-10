617cb4d4e2b3841b0e9ce30fe113a5c42f0c4cc8
RemiSaurel <r.saurel5@gmail.com>
2023-09-10 12:16:17 +0200

    add: router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
+import router from "./router/routes";
 
-const app = createApp(App)
-app.use(VueQueryPlugin)
-app.mount('#app')
+const app = createApp(App);
+app.use(VueQueryPlugin);
+app.use(router);
+app.mount("#app");
---
+++ src/router/routes.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../pages/HomePage.vue";
+import AllSongsPage from "../pages/AllSongsPage.vue";
+import ArtistPage from "../pages/ArtistPage.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomePage,
+  },
+  {
+    path: "/all",
+    name: "AllSongs",
+    component: AllSongsPage,
+  },
+  {
+    path: "/artist/:artist",
+    name: "Artist",
+    component: ArtistPage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
