57d04bc7cd7f5cb56fcd405673cced9d826b9b0e
RemiSaurel <r.saurel5@gmail.com>
2023-11-03 06:40:25 +0100

    add: loading on artistpage

--- src/pages/ArtistPage.vue
+++ src/pages/ArtistPage.vue
@@ -12,7 +12,16 @@ const { data, isLoading, error } = useArtist(artistName);
 </script>
 
 <template>
-  <div v-if="isLoading">Loading...</div>
+  <div
+    v-if="isLoading"
+    class="flex flex-col w-full h-full m-auto justify-center items-center gap-8"
+  >
+    <div
+      class="animate-spin rounded-full h-16 w-16 border-b-2 border-neutral-900"
+    ></div>
+    <span class="text-2xl font-bold">Chargement...</span>
+  </div>
+
   <div class="flex flex-col gap-2" v-else-if="data">
     <div class="text-3xl font-bold">{{ data.name }}</div>
     <div class="flex gap-8">
