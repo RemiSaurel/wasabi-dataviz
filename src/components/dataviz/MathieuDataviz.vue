c4160a166aa3b3ab58dc02766852139aa15c3177
Mathieu <mathieu.echarte1234@gmail.com>
2023-11-19 22:18:29 +0100

    Ajout du zoom sur un artist lorsqu'on click et lorsqu'on se déplace sur un cercle ajout d'information aussi. MAJ de la BD pour ajouter si c'est un artiste solo ou en group + ajout de coloration en fonction de cela

--- src/components/dataviz/MathieuDataviz.vue
+++ src/components/dataviz/MathieuDataviz.vue
@@ -1,28 +1,42 @@
 <script setup lang="ts">
 import * as d3 from "d3";
 import { ref, onMounted } from "vue";
+import {formatNumber} from "../../utils/functions";
 
 const data = ref(null);
-const isLoading = ref(true);
-
+let artistName = ref("");
+let nbAlbums =ref(null);
+let popularity=ref(null);
+let selectedArtistInfo = ref(null);
+// Create a tooltip element with some styles
+const tooltip = d3
+    .select("body")
+    .append("div")
+    .attr(
+        "class",
+        "tooltip bg-neutral-800 text-lg text-white pt-1 p-2 rounded-md",
+    )
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("visibility", "hidden");
 // Read the data
-onMounted(async () => {
+onMounted(async () =>
+{
   // Import data from file
   const response = await fetch(import.meta.env.BASE_URL + "data/artist_mathieu.json");
   // Transform our data to a json object
   data.value = await response.json();
 
   // Take only the first 1000 data points
-  data.value = data.value.slice(0, 1000);
+  data.value = data.value.slice(0, 10000);
   data.value = data.value.filter((d) => {
-    return d.carrier.begin != 0
+    return d.carrier.begin > 1900
   })
 
   // set the dimensions and margins of the graph
-  const margin = { top: 100, right: 50, bottom: 120, left: 50 };
-  const width = 1500 - margin.left - margin.right;
-  const height = 420 - margin.top - margin.bottom;
-
+  const margin = { top: 100, right: 100, bottom: 120, left: 50 };
+  const width = 1000 - margin.left - margin.right;
+  const height = 600 - margin.top - margin.bottom;
   // append the svg object to the body of the page
   const svg = d3.select("#chart")
       .attr("width", width + margin.left + margin.right)
@@ -33,10 +47,9 @@ onMounted(async () => {
   // Add X axis
   const x = d3.scaleLinear()
       .domain([
-        1900,
-        d3.max(data.value, d => d.carrier.end),
+        0,10,15,20,30,40,50,60,70,80,90
       ])
-      .range([0, width/2]);
+      .range([0, width/15]);
 
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
@@ -57,7 +70,7 @@ onMounted(async () => {
   svg.append("text")
       .attr("transform", "translate(" + (width / 4) + " ," + (margin.top+height-40) + ")")
       .style("text-anchor", "middle")
-      .text("Carrier Begins");
+      .text("Career Length");
 
   // Add Y axis label
   svg.append("text")
@@ -83,18 +96,100 @@ onMounted(async () => {
       .data(data.value)
       .enter()
       .append("circle")
-      .attr("cx", d => (d.carrier && d.carrier.begin) ? x(d.carrier.begin) : 0)
+      .attr("cx", d => (d.carrier && d.carrier.begin) ? x(d.carrier.end-d.carrier.begin) : 0)
       .attr("cy", d => (d.albums && d.albums.length) ? y(d.albums.length) : 0)
       .attr("r", d => (d.popularity) ? z(d.popularity) : 0)
-      .style("fill", d => (d.popularity && d.popularity > 40000) ? "red" : "#69b3a2")  // Change color if popularity > 40000
+      .style("fill", d => (d.members && d.members.length > 0) ? "red" : "#69b3a2")  // Change color if artist solo or not
       .style("opacity", "0.7")
-      .attr("stroke", "black");
-});
+      .attr("stroke", "black")
+      .on("click", function (event, d) {
+        // Country on the map should be pinging
+        d3.select(this).attr("fill", "#000000");
+        selectedArtistInfo.value = d;
+      })
+      .on("mouseenter", function (event, d) {
+        setupTooltip(tooltip, event, d);
+        artistName = d.artist;
+        nbAlbums = d.albums.length;
+        popularity = d.popularity;
+        d3.select(this).attr("fill", "#FFFF00");
+        // Cursor becomes a pointer
+        d3.select(this).attr("cursor", "pointer");
+      })
+      .on("mouseleave", function (event, d) {
+        // Hide and clear the tooltip
+        tooltip.style("visibility", "hidden");
+        // Reset the color when mouse leaves
+        d3.select(this).attr("fill", d => (d.members && d.members.length > 0) ? "red" : "#69b3a2");
+      });
 
+});
+const setupTooltip = (tooltip, event, d) => {
+  // Show the tooltip next to the cursor
+  const cursor = d3.pointer(event, window);
+
+  // Get info to display in tooltip
+  const circleTootltipInfo = {
+    Artistname: d.artist,
+    nbAlbums: formatNumber(d.albums.length),
+    popularity: formatNumber(d.popularity),
+  };
+
+  // Styles for the tooltip
+  tooltip
+      .style("visibility", "visible")
+      .style("left", cursor[0] + 10 + "px")
+      .style("top", cursor[1] + 10 + "px");
+
+  // Display tooltip
+  tooltip.html(`
+        <div class="flex flex-col p-1 gap-1">
+          <span class="text-xl font-bold">${circleTootltipInfo.Artistname}</span>
+          <span class="w-full h-[2px] bg-white"></span>
+          <span>${circleTootltipInfo.popularity} followers(s)</span>
+          <span>${circleTootltipInfo.nbAlbums} album(s)</span>
+        </div>
+      `);
+};
 </script>
 
 <template>
-  <svg id="chart" />
+  <div class="container">
+    <div class="chart-container">
+      <svg id="chart" />
+    </div>
+    <div class="artist-info-container" v-if="selectedArtistInfo">
+      <span class="text-xl font-bold">{{ selectedArtistInfo.artist }}</span>
+      <span class="w-full h-[2px] bg-white"></span>
+      <ul>
+        <span>{{ formatNumber(selectedArtistInfo.popularity) }} follower(s)</span>
+      </ul>
+      <span>{{ formatNumber(selectedArtistInfo.albums.length) }} album(s)</span>
+      <div v-if="selectedArtistInfo.members && selectedArtistInfo.members.length">
+        <h3>Members:</h3>
+        <ul>
+          <li v-for="member in selectedArtistInfo.members" :key="member">{{ member }}</li>
+        </ul>
+      </div>
+      <div v-if="selectedArtistInfo.albums && selectedArtistInfo.albums.length">
+        <h3>Albums:</h3>
+        <ul>
+          <li v-for="album in selectedArtistInfo.albums" :key="album">{{ album }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex; /* Utilise Flexbox pour aligner les éléments en ligne */
+}
+.artist-info-container {
+  float: right; /* Float the artist info container to the right */
+  border: 10px solid #213547; /* Adjust the border style as needed */
+  padding: 10px; /* Adjust the padding as needed */
+  border-radius: 5px; /* Optional: Add border-radius for rounded corners */
+  top: 100px;
+}
+</style>
